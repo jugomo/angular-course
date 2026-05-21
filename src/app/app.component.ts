91c385a2101b0c8515442ce6b60eada773bc46f1
julio <jugomo18@gmail.com>
2026-05-21 12:30:13 +0200

    19. 15 Angular Built-In Pipes - Complete Catalog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,16 @@ export class AppComponent {
     courses = COURSES;
     // courses = [];
 
+    startDate = new Date(2000, 0, 1);
+
+    title = COURSES[0].description;
+
+    price = 9.99123456;
+
+    rate = 0.67;
+
+    course = COURSES[0];
+
     onCourseSelected(course: Course) {
         console.log("Card clicked", course);
     }
